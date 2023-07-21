5659f5dfee6bcd90ed6d809ec4e8b824b8b8205d
alfredocloud <alfredo.snow1@gmail.com>
2023-07-21 17:21:11 -0600

    add validation

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 function enviarSlack() {
-  var info = document.getElementById("message").value;
-  var nombre = document.getElementById("name").value;
-  var correo = document.getElementById("email").value;
-  var numero = document.getElementById("surname").value;
-  var DatosEnvio="Nombre: "+ nombre +"\n Correo: "+correo+ "\n Número telefónico: " + numero + '\n\n Mensaje: ' +  info  
 
+  // var nombre = document.getElementById("name").value;
+  // var numero = document.getElementById("phone").value;
+  var correo = document.getElementById("email").value;
+  var info = document.getElementById("message").value;
+
+  // var DatosEnvio = "Nombre: " + nombre + "\n Correo: " + correo + "\n Número telefónico: " + numero + '\n\n Mensaje: ' + info
+
+  var DatosEnvio = "Correo: " + correo + '\n\n Mensaje: ' + info
+
+  var send_slack = localStorage.getItem('send_slack') || false;
+
+  if (send_slack === false) {
+    Send_Request(DatosEnvio);
+    localStorage.setItem('send_slack', true);
+
+    // document.getElementById("name").value = "";
+    // numero = document.getElementById("phone").value = "";
+
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+
+
+    // Pop-up
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+}
+
+function decodeBase64(base64Str) {
+  var decodedStr = atob(base64Str);
+  var originalStr = decodeURIComponent(escape(decodedStr));
+  return originalStr;
+}
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Send_Request(DatosEnvio) {
   var myHeaders = new Headers();
   myHeaders.append("Content-type", "application/json");
   myHeaders.append("Access-Control-Allow-Origin", "*");
@@ -25,29 +70,7 @@ function enviarSlack() {
   var url = decodeBase64(url_base64)
   fetch(url, requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
+    .then(result => console.log("result"))
     .catch(error => console.log('error', error));
 
-       var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-  }
-
-function decodeBase64(base64Str) {
-  var decodedStr = atob(base64Str);
-  var originalStr = decodeURIComponent(escape(decodedStr));
-  return originalStr;
-}
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
 }
